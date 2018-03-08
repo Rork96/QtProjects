39864ca26b5a42774288ea11bdb6f8adb427ac88
masterarrow <36331175+masterarrow@users.noreply.github.com>
2018-03-08 20:49:25 +0200

    Add files via upload

---
+++ QImageWiever/scripts/QImageViewer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=`pwd`/lib
+export QML_IMPORT_PATH=`pwd`/qml
+export QML2_IMPORT_PATH=`pwd`/qml
+export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/plugins/platforms
+./QImageWiever

69c4030e634df05067fba93c101ef4570fb7afdb
masterarrow <masterarrows@gmail.com>
2018-03-22 14:05:00 +0200

    Release

---
+++ Tests/Run_tests.bat
@@ -0,0 +1,17 @@
+@echo off
+color 02
+echo.
+echo Test1
+echo.
+%CD%\Windows\MyClass\Test1.exe
+echo.
+echo Test2
+echo.
+%CD%\Windows\MyClass\Test2.exe
+echo.
+echo Test3
+echo.
+%CD%\Windows\MyClass\Test3.exe
+echo.
+::pause
+exit

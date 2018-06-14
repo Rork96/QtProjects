15fb7a0ffc8997f8113ed2026a3153a3d35bc53a
masterarrow <masterarrow@gmail.com>
2018-06-14 19:44:19 +0300

    Builder

---
+++ Builder/Builder/createlogoform.h
@@ -0,0 +1,22 @@
+#ifndef CREATELOGOFORM_H
+#define CREATELOGOFORM_H
+
+#include <QWidget>
+
+namespace Ui {
+    class CreateLogoForm;
+}
+
+class CreateLogoForm : public QWidget
+{
+Q_OBJECT
+
+public:
+    explicit CreateLogoForm(QWidget *parent = 0);
+    ~CreateLogoForm();
+
+private:
+    Ui::CreateLogoForm *ui;
+};
+
+#endif // CREATELOGOFORM_H
